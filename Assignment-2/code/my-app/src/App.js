1f321941ced25fb4cd8130b97ac0beea4541325f
VAMAN KUMAR TAMARANA <tamaranavamankumar@gmail.com>
2022-02-05 10:44:24 +0530

    React Assignment-2 submission

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -4,11 +4,33 @@ import './App.css';
 
 
 function App(props) {
- const {employee}=props;
+ const {employee, title}=props;
     
 return(
-  <div className="container">
-  {/* Start your React code here */}
+  <div className="container App">
+    <div className="profile-image">
+      <img className="profile" src={employee.profileImg} width="100" height="100" alt="profileimage" />
+    </div>
+
+    <div className="title">
+      <h1>{employee.name}</h1>
+    </div>
+
+    <div className="profile-content">
+      <label>Location</label>
+      <strong>{employee.location}</strong>
+    </div>
+
+    <div className="profile-content">
+      <label>Blood group</label>
+      <strong>{employee.bloodGroup}</strong>
+    </div>
+
+    <div className="profile-content">
+      <label>Age</label>
+      <strong>{employee.age}</strong>
+    </div>
+  
 </div>
 )
 }
